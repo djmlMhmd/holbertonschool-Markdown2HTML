5ba17f306f4eb45efc3f8d3ac079c95d8e807949
djamalMhmd <djamaldine-mohamed@live.fr>
2023-12-16 03:32:17 +0100

    change function

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script converting Markdown markup language files into HTML files"""
+"""Un script pour convertir des fichiers écrits en langage Markdown en fichiers HTML"""
 
 import hashlib
 import os
@@ -8,39 +8,39 @@ import sys
 
 
 def replace_bold(match):
-    """A function that returns a formatted bold string html"""
+    """Une fonction qui retourne une chaîne en gras formatée en HTML"""
     bold_text = match.group(1)
     return f'<b>{bold_text}</b>'
 
 
 def replace_em(match):
-    """A function that returns a formatted em string html"""
+    """Une fonction qui retourne une chaîne en italique formatée en HTML"""
     em_text = match.group(1)
     return f'<em>{em_text}</em>'
 
 
 def encode_md5(match):
-    """A function that returns a MD5 encoded string"""
+    """Une fonction qui retourne une chaîne encodée en MD5"""
     text_to_encode = match.group(1)
     return hashlib.md5(text_to_encode.encode()).hexdigest()
 
 
 def remove_c(match):
-    """A function that returns a string without "c" characters"""
+    """Une fonction qui retourne une chaîne sans les caractères 'c'"""
     text_with_c = match.group(1)
     return text_with_c.replace('c', '').replace('C', '')
 
 
 def main():
-    """The main function running the script
+    """La fonction principale qui exécute le script
 
-    Raises:
-            "Usage: ./markdown2html.py README.md README.html" to STDERR:
-            If the number of arguments in the CLI is different of 2
-            "Missing <filename>" to STDERR: If the Markdown file doesn’t exist
+    Soulève:
+            "Usage: ./markdown2html.py README.md README.html" dans STDERR:
+            Si le nombre d'arguments dans la CLI est différent de 2
+            "Fichier <nom_du_fichier> manquant" dans STDERR: Si le fichier Markdown n'existe pas
     """
 
-    # Checking if number of CLI arguments is wright
+    # Vérifie si le nombre d'arguments CLI est correct
     if len(sys.argv) != 3:
         print("Usage: ./markdown2html.py README.md README.html",
               file=sys.stderr)
@@ -49,41 +49,41 @@ def main():
     md_file = sys.argv[1]
     html_file = sys.argv[2]
 
-    # Checking if markdown file exists
+    # Vérifie si le fichier markdown existe
     if not os.path.isfile(md_file):
-        print(f"Missing {md_file}", file=sys.stderr)
+        print(f"Fichier manquant {md_file}", file=sys.stderr)
         sys.exit(1)
 
-    # Parsing the markdown file and writing html to a new html file
+    # Analyse le fichier markdown et écrit le HTML dans un nouveau fichier HTML
     with open(md_file, "r", encoding='UTF-8') as markdown:
         with open(html_file, "w", encoding='UTF-8') as html:
 
-            # HTML tags
+            # Balises HTML
             ol_open = False
             ul_open = False
             p_open = False
 
-            # Special patterns
+            # Modèles spéciaux
             pattern_bold = re.compile(r'\*\*(.*?)\*\*')
             pattern_em = re.compile(r'__(.*?)__')
             pattern_md5 = re.compile(r'\[\[(.*?)\]\]')
             pattern_c = re.compile(r'\(\((.*?)\)\)')
 
-            # The loop to parse the file
+            # La boucle pour analyser le fichier
             while True:
                 line = markdown.readline()
 
-                # Checks if we are in an ul
+                # Vérifie si on est dans une liste non ordonnée
                 if ul_open and not line.startswith("-"):
                     html.write("</ul>\n")
                     ul_open = False
 
-                # Checks if we are in an ol
+                # Vérifie si on est dans une liste ordonnée
                 if ol_open and not line.startswith("* "):
                     html.write("</ol>\n")
                     ol_open = False
 
-                # Checks if we are in a paragraph
+                # Vérifie si on est dans un paragraphe
                 if p_open and (line.startswith("-") or
                                line.startswith("* ,") or
                                line.startswith("#") or
@@ -91,24 +91,23 @@ def main():
                     html.write("</p>\n")
                     p_open = False
 
-                # Reaches the EOF
+                # Atteint la fin du fichier (EOF)
                 if not line:
                     break
 
-                # Evaluation em, bold, md5, strip "c"
-                # chars contents in the line
+                # Évalue et modifie le contenu de la ligne avec les balises em, bold, md5, et enlève les caractères "c"
                 line = re.sub(pattern_bold, replace_bold, line)
                 line = re.sub(pattern_em, replace_em, line)
                 line = re.sub(pattern_md5, encode_md5, line)
                 line = re.sub(pattern_c, remove_c, line)
 
-                # Generates Headings
+                # Génère les titres
                 if line.startswith("#"):
                     count = line.count("#")
                     text = line.rsplit('# ', 1)[1].strip()
                     html.write(f"<h{count}>{text}</h{count}>\n")
 
-                # Generates unordered listing
+                # Génère une liste non ordonnée
                 elif line.startswith("-"):
                     if not ul_open:
                         html.write("<ul>\n")
@@ -116,27 +115,9 @@ def main():
                     text = line.rsplit('- ', 1)[1].strip()
                     html.write(f"<li>{text}</li>\n")
 
-                # Generates ordered listing
+                # Génère une liste ordonnée
                 elif line.startswith('* '):
                     if not ol_open:
                         html.write("<ol>\n")
                         ol_open = True
-                    text = line.rsplit('* ', 1)[1].strip()
-                    html.write(f"<li>{text}</li>\n")
-
-                # Generates paragraphs
-                elif not line.startswith("\n"):
-                    if not p_open:
-                        html.write("<p>\n")
-                        p_open = True
-                    else:
-                        html.write("<br/>\n")
-                    text = line
-                    html.write(text)
-
-    # Exits after sucessful program execution
-    sys.exit()
-
-
-if __name__ == "__main__":
-    main()
+                    text = line.rsplit('*
